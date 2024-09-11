63f5090753a0ccb3794d51e2149fb5fac567d8a9
Onela25 <onelawellem0525@gmail.com>
2024-09-11 11:02:19 +0200

    Made changes to pages

---
+++ book.js
@@ -0,0 +1,9 @@
+
+        function openForm() {
+            document.getElementById("bookingForm").style.display = "block";
+        }
+
+        function closeForm() {
+            document.getElementById("bookingForm").style.display = "none";
+        }
+    
---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $event = $_POST['event'];
+    $date = $_POST['date'];
+    $age = $_POST['age'];
+
+    $to = "masethe247@gmail.com";
+    $subject = "New Booking Request";
+    $message = "Name: $name\nEmail: $email\nPhone: $phone\nEvent: $event\nDate: $date\nAge: $age";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Booking request sent successfully!";
+    } else {
+        echo "Failed to send booking request.";
+    }
+}
+?>
