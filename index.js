97ff662996b055e87e2dfabac22489bfe034dd26
Onela25 <onelawellem0525@gmail.com>
2024-10-04 11:21:38 +0200

    fixed the alert prompt

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navLinks = document.querySelector('.nav-links');
+    const navLinks = document.getElementById('nav-links');
     const backButton = document.getElementById('back-button');
     const hamburger = document.querySelector('.hamburger');
 
+    
+    function verifyAge() {
+        console.log("verifyAge function called");
+        const age = prompt("How old are you?");
+        console.log("User's age:", age);
+        if (age < 18) {
+            alert("The legal age to drink is 18. Sorry ☹️."); 
+        } else {
+            alert("Welcome🥂"); 
+        }
+    }
 
-    if (toggleButton) {
-        toggleButton.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
+    
+    verifyAge();
+
+    
+    if (hamburger) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active'); 
+            hamburger.classList.toggle('active'); 
         });
     }
+
+    if (backButton) {
+        backButton.addEventListener('click', () => {
+            window.location.href = 'index.html'; 
+        });
+    }
+
+    
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            backButton.style.display = 'block'; 
+            localStorage.setItem('navLinkClicked', 'true');
+        });
+    });
 
     
     if (localStorage.getItem('navLinkClicked')) {
         backButton.style.display = 'block';
     } else {
         backButton.style.display = 'none';
-    }
-
-    document.querySelectorAll('.nav-links a').forEach(link => {
-        link.addEventListener('click', () => {
-            backButton.style.display = 'block';
-            localStorage.setItem('navLinkClicked', 'true');
-        });
-    });
-
-    backButton.addEventListener('click', () => {
-        window.location.href = 'index.html';
-    });
-
-    
-    if (hamburger) {
-        hamburger.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-            hamburger.classList.toggle('active');
-        });
-    }
-
-    
-    function verifyAge() {
-        console.log("verifyAge function called");
-        const age = prompt("How old are you?");
-        if (age < 18) {
-            alert("The legal age to drink is 18. Sorry ☹️.");
-        } else {
-            alert("Welcome🥂");
-        }
-    }
-
-    if (!localStorage.getItem('ageVerified')) {
-        verifyAge();
-        localStorage.setItem('ageVerified', 'true'); 
     }
 });
