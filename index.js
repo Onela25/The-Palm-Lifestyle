46782fbf0cdf69be03d5f0e7261627890bae906a
Onela25 <onelawellem0525@gmail.com>
2024-10-04 09:50:41 +0200

    Made the hamburger menu to appear on small screens and ensured functionality

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navLinks = document.querySelector('.nav-links');
+    const navLinks = document.getElementById('nav-links');
     const backButton = document.getElementById('back-button');
     const hamburger = document.querySelector('.hamburger');
 
-   
-    if (toggleButton) {
-        toggleButton.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-    }
-
-   
-    if (localStorage.getItem('navLinkClicked')) {
-        backButton.style.display = 'block';
-    } else {
-        backButton.style.display = 'none';
-    }
-
-    document.querySelectorAll('.nav-links a').forEach(link => {
-        link.addEventListener('click', () => {
-            backButton.style.display = 'block';
-            localStorage.setItem('navLinkClicked', 'true');
-        });
-    });
-
-    backButton.addEventListener('click', () => {
-        window.location.href = 'index.html';
-    });
-
-  
-    if (hamburger) {
-        hamburger.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-            hamburger.classList.toggle('active');
-        });
-    }
-
-   
+    
     function verifyAge() {
+        console.log("verifyAge function called");
         const age = prompt("How old are you?");
+        console.log("User's age:", age);
         if (age < 18) {
             alert("The legal age to drink is 18. Sorry ☹️.");
         } else {
@@ -49,4 +17,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
     verifyAge();
+
+   
+    if (hamburger) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active'); 
+            hamburger.classList.toggle('active'); 
+        });
+    }
+
+   
+    if (backButton) {
+        backButton.addEventListener('click', () => {
+            window.location.href = 'index.html';
+        });
+    }
+
+   
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            backButton.style.display = 'block';
+            localStorage.setItem('navLinkClicked', 'true'); 
+        });
+    });
+
+   
+    if (localStorage.getItem('navLinkClicked')) {
+        backButton.style.display = 'block';
+    } else {
+        backButton.style.display = 'none';
+    }
 });
